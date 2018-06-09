63d8cde41e96db20531220c0f510081c28a5ed11
ZWQ <zhong0102@qq.com>
2018-06-09 23:18:27 +0800

    增加代理

---
+++ src/home.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="home">
+    <el-button @click="dialogVisible = true">新增</el-button>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose" center>
+      <span class="btns">
+        <el-button type="primary" @click="toListInfo">预售</el-button>
+        <el-button type="primary" @click="dialogVisible = false">直采</el-button>
+      </span>
+    </el-dialog>
+
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="name" label="商品名称" width="180">
+      </el-table-column>
+      <el-table-column prop="type" label="商品来源" width="180">
+      </el-table-column>
+      <el-table-column prop="stock" label="库存">
+      </el-table-column>
+      <el-table-column prop="sold" label="已售">
+      </el-table-column>
+      <el-table-column prop="publishTime" label="上架时间">
+      </el-table-column>
+      <el-table-column prop="lowerTime" label="下架时间">
+      </el-table-column>
+      <el-table-column prop="status" label="当前状态">
+      </el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看详情</el-button>
+          <el-button type="text" size="small">发布</el-button>
+          <el-button type="text" size="small">隐藏</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      tableData: [
+        {
+          id: "",
+          status: "",
+          type: "",
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+          stock: "",
+          sold: "",
+          publishTime: "",
+          lowerTime: "",
+          isShow: ""
+        }
+      ],
+      dialogVisible: false
+    };
+  },
+  methods: {
+    handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
+      },
+    toListInfo(){
+      this.dialogVisible =  false
+      this.$router.push({ path: "/listinfo" });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#home {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  // display: flex;
+  // flex-direction: column;
+}
+.btns{
+  margin: 0 auto;
+}
+</style>
---
+++ src/signIn.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="login" class="login">
+    <div class="formWrap">
+      <h3>登陆</h3>
+      <el-form status-icon label-width="80px" :label-position="'left'">
+        <el-form-item label="用户名">
+          <el-input type="username" v-model="formDate2.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input type="password" v-model="formDate2.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" v-on:click.prevent="submitForm2">提交</el-button>
+          <el-button @click="formVisible = true">
+            <router-link to="/signup">注册</router-link>
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      formDate2: {
+        userName: "",
+        password: ""
+      },
+      formVisible: true
+    };
+  },
+  methods: {
+    resetForm() {
+      for (let key in this.formDate) {
+        this.formDate[key] = "";
+      }
+    },
+    submitForm2() {
+      let password = this.$md5(this.formDate2.password);
+      // let json = {password: password,userName:this.formDate2.userName}
+
+      let json = JSON.stringify({
+        password: password,
+        userName: this.formDate2.userName
+      });
+      console.log(json, "json");
+      this.$axios.defaults.headers.post['Content-Type'] = 'application/json';
+      this.$axios.post('/api/api/user/admin/adminLogin', {
+          "userName": this.formDate2.userName,
+          "password": password
+        }).then(res => {
+          console.log(res);
+        })
+
+    
+    }
+  }
+};
+</script>
+style
+<style lang="scss">
+.login {
+  width: 100%;
+  height: 100%;
+  > .formWrap {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 300px;
+    box-shadow: 0px 0px 5px #888888;
+    padding: 24px 32px;
+    border-radius: 10px;
+    transform: translate(-50%, -50%);
+    > h3 {
+      text-align: center;
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
